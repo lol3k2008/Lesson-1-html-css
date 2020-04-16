9d7135a6ee46e2d3ec7ba55bf243adee684ea3d9
Patryk Strzyżewski <63202238+lol3k2008@users.noreply.github.com>
2020-04-16 19:55:59 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,77 @@
+const hamb =document.querySelector (`.men-hamb`);
+const menu =document.querySelector (`.hiden-menu`);
+
+
+hamb.addEventListener(`click`, function(){
+
+
+    menu.classList.toggle(`show-menu`)
+});
+
+
+// // -----------------------MOJE_PRACE---------------------
+
+// const photo1 =document.querySelector(`.pone`);
+// const photo2 =document.querySelector(`.ptwo`);
+// const photo3 =document.querySelector(`.pthree`);
+// const photo4 =document.querySelector(`.pfour`);
+// const photo5 =document.querySelector(`.pfive`);
+// const photo6 =document.querySelector(`.psix`);
+// const photo7 =document.querySelector(`.pseven`);
+// const photo8 =document.querySelector(`.peight`);
+
+
+// photo1.addEventListener(`click`, function(){
+
+//     photo1.classList.toggle(`transF`)
+   
+    
+// });
+
+// photo2.addEventListener(`click`, function(){
+
+//     photo2.classList.toggle(`transF`)
+   
+    
+// });
+// photo3.addEventListener(`click`, function(){
+
+//     photo3.classList.toggle(`transF`)
+   
+    
+// });
+
+// photo4.addEventListener(`click`, function(){
+
+//     photo4.classList.toggle(`transF`)
+   
+    
+// });
+
+// photo5.addEventListener(`click`, function(){
+
+//     photo5.classList.toggle(`transF`)
+   
+    
+// });
+
+// photo6.addEventListener(`click`, function(){
+
+//     photo6.classList.toggle(`transF`)
+   
+    
+// });
+// photo7.addEventListener(`click`, function(){
+
+//     photo7.classList.toggle(`transF`)
+   
+    
+// });
+
+
+// photo8.addEventListener(`click`, function(){
+
+//     photo8.classList.toggle(`transF`)
+   
+    
+// });
